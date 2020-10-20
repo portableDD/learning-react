0b9cb9bc167435110849d9775b8c2fd5ba19c738
portable <emmanueltemitopedorcas20@gmail.com>
2020-10-20 11:51:21 +0100

    create react app

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Ninjas from "./Ninjas"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class  App extends  Component {
+  state={
+    ninjas:[
+      {name:"Ryu", age:"30", belt:"black", id:"1"},
+      {name:"Yoshi", age:"20", belt:"green", id:"2"},
+      {name:"cystal", age:"25", belt:"pink", id:"3"}
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>My first React</h1>
+        <p>welcome :)</p>
+        <Ninjas ninjas={this.state.ninjas} />
+      </div>
+    );
+  }
 }
 
 export default App;
