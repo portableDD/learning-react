f4169be247ad09d8bf999560848032e6acfd76d6
portable <emmanueltemitopedorcas20@gmail.com>
2020-10-26 16:00:31 +0100

    hi dear

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,24 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
+import "./Ninjas.css"
 
-class Ninjas extends Component {
-    render() {
-        // console.log(this.props);
-        const { ninjas } = this.props;
+const Ninjas = ({ninjas, deleteNinja}) =>{
         const ninjasList = ninjas.map(ninja => {
-            return(
-                <div className="ninja" key={ninja.id}>
-                    <div>Name: { ninja.name } </div>
-                    <div>Age : { ninja.age  } </div>
-                    <div>belt:  { ninja.belt } </div>
-                </div>
-            )
+            if (ninja.age > 20){
+                return(
+                    <div className="ninja" key={ninja.id}>
+                        <div>Name: { ninja.name } </div>
+                        <div>Age : { ninja.age  } </div>
+                        <div>belt:  { ninja.belt } </div>
+                        <button onClick ={() => {deleteNinja(ninja.id)}}>Delete Ninja</button>
+                    </div>
+                )
+            } else {
+                return null
+            }
         })
         return(
            <div className="ninja-list">
                {  ninjasList }
            </div>
         )
-    }
 }
 
 export default Ninjas
